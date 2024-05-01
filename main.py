5dfa67f86762b6537efedf2dd4e4ad5b092b4d2c
flack <163582560+FlackCode@users.noreply.github.com>
2024-05-01 08:20:53 +0200

    Movement + Wall colors

--- main.py
+++ main.py
@@ -8,23 +8,41 @@ mapArray = [[1,1,1,1,1],
             [1,0,0,0,1],
             [1,1,1,1,1],]
 
+for i in range(len(mapArray[0])):
+    for j in range(len(mapArray[1])):
+        if mapArray[i][j] == 1:
+            mapArray[i][j] = list(np.random.uniform(0, 1 ,3))
+
 xPos, yPos = (1,1)
 xExit, yExit = (3,3)
 playerRotation = np.pi/4
 
 while True:
+    plt.hlines(-0.6, 0, 60, colors='gray', lw=165, alpha=0.5)
+    plt.hlines(0.6, 0, 60, colors='lightblue', lw=165, alpha=0.5)
+    xTile, yTile, ceilTile = ([], [], [])
+
     for i in range(60):
         playerRotation_i = playerRotation + np.deg2rad(i-30)
         x, y = (xPos, yPos)
         sin, cos = (0.02*np.sin(playerRotation_i), 0.02*np.cos(playerRotation_i))
         n = 0
         while True:
+            xx, yy = (x, y)
             x, y = (x+cos, y+sin)
             n = n+1
+            if abs(int(3*xx)-int(3*x)) > 0 or abs(int(3*yy)-int(3*y)) > 0:
+                xTile.append(i)
+                yTile.append(-1 / (0.02 * n))
+                if int(x) == xExit and int(y) == yExit:
+                    ceilTile.append('b')
+                else:
+                    ceilTile.append('k')
             if mapArray[int(x)][int(y)] != 0:
-                height = 1 / (0.02 * n)
+                height = np.clip(1 / (0.02 * n), 0, 1)
+                color = np.asarray(mapArray[int(x)][int(y)])*(0.3 + 0.7 * height**2)
                 break
-        plt.vlines(i, -height, height, lw=8)
+        plt.vlines(i, -height, height, lw=8, colors=color)
     plt.axis('off'); plt.tight_layout(); plt.axis([0, 60, -1, 1])
     plt.draw(); plt.pause(0.0001); plt.clf()
 
